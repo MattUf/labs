c788fa169890752dd100e22411f81719e4a8dbff
MatthewC00273575 <ufumeli2@gmail.com>
2022-09-20 10:44:52 +0100

    Initial commit.`

---
+++ labs/src/firstLab0/lab0Class.java
@@ -0,0 +1,12 @@
+package firstLab0;
+
+public class lab0Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, World");
+
+	}
+
+}
